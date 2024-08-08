5658704218c3e6e10ad39033b08f535fc0542b70
SealCia1123 <maoduchai404@gmail.com>
2024-08-08 13:34:07 +0700

    update

--- script/btl.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    let dmb = document.querySelectorAll("button.darkmode");
-        for (db of dmb){
-            db.onclick = function(){
-            let bg = document.getElementById("headbg");
-            bg.style.background="linear-gradient(purple, violet)";
-        }
-    }
-}
