e1b93400eb87795fd1e8b7b68d6c194f0a811511
SealCia1123 <maoduchai404@gmail.com>
2024-08-08 17:24:50 +0700

    update layout

--- script/home.js
+++ script/home.js
@@ -10,6 +10,5 @@ window.onload = function () {
     let leftButton = document.getElementById("left-arrow");
     leftButton.onclick = function () {
         console.log("1");
-        document.getElementsByClassName("movies").scrollLeft -= 20;
     };
 };
