cd153265cf6ad801bcffded90b6cfec44cbe5562
SealCia1123 <maoduchai404@gmail.com>
2024-08-08 15:40:53 +0700

    update

--- script/home.js
+++ script/home.js
@@ -1,9 +1,15 @@
 window.onload = function () {
-    let dmb = document.querySelectorAll("button.darkmode");
-    for (db of dmb) {
-        db.onclick = function () {
-            let bg = document.getElementById("headbg");
-            bg.style.background = "linear-gradient(purple, violet)";
-        };
-    }
+    // let dmb = document.querySelectorAll("button.darkmode");
+    // for (db of dmb) {
+    //     db.onclick = function () {
+    //         let bg = document.getElementById("headbg");
+    //         bg.style.background = "linear-gradient(purple, violet)";
+    //     };
+    // }
+
+    let leftButton = document.getElementById("left-arrow");
+    leftButton.onclick = function () {
+        console.log("1");
+        document.getElementsByClassName("movies").scrollLeft -= 20;
+    };
 };
