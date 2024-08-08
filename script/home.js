5bd960d8d1b2fc5fbeb41a77d95571065813ee90
SealCia1123 <maoduchai404@gmail.com>
2024-08-08 14:18:46 +0700

    update

--- script/home.js
+++ script/home.js
@@ -1,9 +1,9 @@
-window.onload = function(){
+window.onload = function () {
     let dmb = document.querySelectorAll("button.darkmode");
-        for (db of dmb){
-            db.onclick = function(){
+    for (db of dmb) {
+        db.onclick = function () {
             let bg = document.getElementById("headbg");
-            bg.style.background="linear-gradient(purple, violet)";
-        }
+            bg.style.background = "linear-gradient(purple, violet)";
+        };
     }
-}
+};
