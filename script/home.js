6c6d449848d55869b01fe237620607d92d624462
SealCia1123 <maoduchai404@gmail.com>
2024-08-11 17:57:56 +0700

    update

--- script/home.js
+++ script/home.js
@@ -1,10 +1,3 @@
 window.onload = function () {
-    let movies = document.querySelectorAll(".movies");
-    let leftButton = document.querySelector("#left-arrow");
-    let rightButton = document.querySelector("#right-button");
 
-
-    rightButton.addEventListener("click", function () {
-        movies.scrollLeft += 900;
-    });
 };
