2ac5975e4619a1565c3f9c6894e7b202945d93f4
SealCia1123 <maoduchai404@gmail.com>
2024-08-11 23:50:06 +0700

    update link to img

--- script/home.js
+++ script/home.js
@@ -1,15 +1,15 @@
 window.onload = function () {
-	// Thêm class animate__animated vào các div overlay
-	let addAni = document.querySelectorAll('.overlay');
-	addAni.forEach(function (each) {
-		each.classList.add('animate__animated');
-	});
-
-	// Duyệt qua các class hover-img và thêm eventListener khi di chuột vào thì toggle class animate__zoomIn
-	let hoverImages = document.querySelectorAll('.hover-img');
-	for (let i = 0; i < hoverImages.length; i++) {
-		hoverImages[i].addEventListener('mouseenter', () => {
-			document.querySelectorAll('.overlay')[i].classList.toggle('animate__zoomIn');
-		});
-	}
+	// // Thêm class animate__animated vào các div overlay
+	// let addAni = document.querySelectorAll('.overlay');
+	// addAni.forEach(function (each) {
+	// 	each.classList.add('animate__animated');
+	// });
+	//
+	// // Duyệt qua các class hover-img và thêm eventListener khi di chuột vào thì toggle class animate__zoomIn
+	// let hoverImages = document.querySelectorAll('.hover-img');
+	// for (let i = 0; i < hoverImages.length; i++) {
+	// 	hoverImages[i].addEventListener('mouseenter', () => {
+	// 		document.querySelectorAll('.overlay')[i].classList.toggle('animate__zoomIn');
+	// 	});
+	// }
 };
