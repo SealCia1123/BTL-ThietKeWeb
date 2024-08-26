074458ba780c79ed10b398f33ad87a2db7dba306
SealCia1123 <maoduchai404@gmail.com>
2024-08-26 23:00:16 +0700

    update

--- script/home.js
+++ script/home.js
@@ -1,5 +1,37 @@
 window.onload = function () {
-    let movies = document.querySelectorAll('.movie');
+    // Tìm kiếm phim
+    const movies = document.querySelectorAll('.movie');
+    const movieNames = document.querySelectorAll('.name a');
+    let search = document.querySelector('#search');
+    const searchButton = document.querySelector('#search-button');
+    let userInput = search.value;
+
+    search.addEventListener('keyup', () => {
+        userInput = search.value;
+        console.log(userInput);
+    });
+
+    searchButton.addEventListener('click', function () {
+        // Xu ly chuoi nguoi dung nhap
+        userInput = new String(userInput);
+        userInput = userInput.toUpperCase();
+        // Kiem tra chuoi
+        if (userInput === '') alert('Vui lòng nhập tên phim!');
+        else {
+            for (let name of movieNames) {
+                let stringName = new String(name.innerText);
+                stringName = stringName.replace(/ *\([^)]*\) */g, '');
+                // Xu ly tim thay
+                if (stringName.includes(userInput)) {
+                    alert('tim thay');
+                    return;
+                }
+            }
+            alert('Khong tim thay');
+        }
+    });
+
+    // Lướt vùng phim theo chiều ngang
     let horizontalZone = document.querySelectorAll('.movies');
     for (let h of horizontalZone) {
         h.addEventListener('wheel', function (e) {
@@ -10,6 +42,7 @@ window.onload = function () {
         });
     }
 
+    // Slideshow ảnh bìa
     let slideIndex = 0;
     showSlides();
     function showSlides() {
