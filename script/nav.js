a3fedb1350e3a1b2449b515855e919035051c5f8
SealCia1123 <maoduchai404@gmail.com>
2024-08-29 19:51:56 +0700

    update

--- script/nav.js
+++ script/nav.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     // Xử lý ẩn hiện nav trên mobile
     $('#show-button').click(function () {
-        $('.nav-mobile-container').animate({ width: 'toggle' }, 300);
+        $('.nav-mobile-container').animate({ width: 'toggle' }, 400);
     });
     $('#close-nav-mobile').click(function () {
-        $('.nav-mobile-container').animate({ width: 'toggle' }, 300);
+        $('.nav-mobile-container').animate({ width: 'toggle' }, 400);
     });
 });
