783e1639d5cff511b3028ab46b3fbb48d5cf5e71
SealCia1123 <maoduchai404@gmail.com>
2024-09-01 22:09:01 +0700

    update Ticket page

--- script/movie-ticket.js
+++ script/movie-ticket.js
@@ -1,1 +1,20 @@
-$(document).ready(function () {});
+window.addEventListener('load', function () {
+    let selectSections = this.document.querySelectorAll('select');
+    for (let s of selectSections) {
+        s.addEventListener('focus', function () {
+            if (s === selectSections[0]) this.setAttribute('size', '3');
+            else this.setAttribute('size', '8');
+            this.setAttribute('style', 'position: absolute');
+        });
+        s.addEventListener('change', function () {
+            this.setAttribute('size', '1');
+            this.setAttribute('style', 'position: relative');
+            this.blur();
+        });
+        s.addEventListener('blur', function () {
+            this.setAttribute('size', '1');
+            this.setAttribute('style', 'position: relative');
+            this.blur();
+        });
+    }
+});
