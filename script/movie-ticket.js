42d7198851ec9614f4ebfe6041a3f54bf494c1ef
SealCia1123 <maoduchai404@gmail.com>
2024-09-02 13:18:44 +0700

    update ticket page

--- script/movie-ticket.js
+++ script/movie-ticket.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function () {
+    // Xử lý chọn rạp, chọn phim không bị chèn dưới các element khác
     let selectSections = this.document.querySelectorAll('select');
     for (let s of selectSections) {
         s.addEventListener('focus', function () {
@@ -15,6 +16,50 @@ window.addEventListener('load', function () {
             this.setAttribute('size', '1');
             this.setAttribute('style', 'position: relative');
             this.blur();
+        });
+    }
+    // Khi ngày vào chọn ngày
+    let dates = this.document.querySelectorAll('.select-date>label');
+    let currentDate = new Date();
+    for (let i = 0; i < dates.length; i++) {
+        dates[i].innerText = `Ngày ${currentDate.getDate() + i + 1}/${currentDate.getMonth() + 1}`;
+    }
+
+    // Xử lý đặt ghế, đặt vé
+    const premiumPrice = 149000;
+    const regularPrice = 79000;
+    const premiumTicket = this.document.querySelector('#premium-ticket');
+    const regularTicket = this.document.querySelector('#regular-ticket');
+    let seats = this.document.querySelectorAll('.seat');
+    let totalPrice = 0;
+    let tickets = 0;
+    let pricePerTicket = premiumPrice;
+    const ticketInput = this.document.querySelectorAll('.ticket-input');
+
+    // Xóa tất cả ghế đã checked
+    function removeCheckSeat(seats) {
+        for (let s of seats) {
+            s.checked = false;
+        }
+    }
+    for (let t of ticketInput) {
+        t.addEventListener('click', function () {
+            // Khi đổi loại vé phòng, xóa tất cả ghế đã check, reset lại số lượng vé, tổng tiền
+            removeCheckSeat(seats);
+            totalPrice = 0;
+            tickets = 0;
+            if (this === premiumTicket) pricePerTicket = premiumPrice;
+            if (this === regularTicket) pricePerTicket = regularPrice;
+        });
+    }
+
+    for (let s of seats) {
+        s.addEventListener('click', function () {
+            // kiểm tra tình trạng ghế để tính số vé, tổng tiền
+            if (this.checked) tickets++;
+            else tickets--;
+            totalPrice = pricePerTicket * tickets;
+            console.log(totalPrice, tickets);
         });
     }
 });
