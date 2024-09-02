670200a3c73fb74bd20bccdff5c97417863a2c44
SealCia1123 <maoduchai404@gmail.com>
2024-09-02 17:05:35 +0700

    finishing project

--- script/movie-ticket.js
+++ script/movie-ticket.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', function () {
     // Xử lý chọn rạp, chọn phim không bị chèn dưới các element khác
-    let selectSections = this.document.querySelectorAll('select');
+    const selectSections = this.document.querySelectorAll('select');
     for (let s of selectSections) {
         s.addEventListener('focus', function () {
             if (s === selectSections[0]) this.setAttribute('size', '3');
-            else this.setAttribute('size', '8');
+            else this.setAttribute('size', '5');
             this.setAttribute('style', 'position: absolute');
         });
         s.addEventListener('change', function () {
@@ -30,9 +30,13 @@ window.addEventListener('load', function () {
     const regularPrice = 79000;
     const premiumTicket = this.document.querySelector('#premium-ticket');
     const regularTicket = this.document.querySelector('#regular-ticket');
-    let seats = this.document.querySelectorAll('.seat');
+    const seats = this.document.querySelectorAll('.seat');
     let totalPrice = 0;
     let tickets = 0;
+    const ticketNumber = this.document.querySelector('.ticket-number');
+    const totalPricePrint = this.document.querySelector('.total-price');
+
+    // Set tiền vé mặc định là premium vì trong html loại vé mặc định được chọn là premium
     let pricePerTicket = premiumPrice;
     const ticketInput = this.document.querySelectorAll('.ticket-input');
 
@@ -46,10 +50,12 @@ window.addEventListener('load', function () {
         t.addEventListener('click', function () {
             // Khi đổi loại vé phòng, xóa tất cả ghế đã check, reset lại số lượng vé, tổng tiền
             removeCheckSeat(seats);
+            ticketNumber.innerText = '0';
+            totalPricePrint.innerText = '0';
             totalPrice = 0;
             tickets = 0;
             if (this === premiumTicket) pricePerTicket = premiumPrice;
-            if (this === regularTicket) pricePerTicket = regularPrice;
+            else if (this === regularTicket) pricePerTicket = regularPrice;
         });
     }
 
@@ -59,7 +65,20 @@ window.addEventListener('load', function () {
             if (this.checked) tickets++;
             else tickets--;
             totalPrice = pricePerTicket * tickets;
-            console.log(totalPrice, tickets);
+            let t = new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'VND',
+            }).format(totalPrice);
+            t = t.replace('₫', '');
+            totalPricePrint.innerText = t;
+            ticketNumber.innerText = tickets;
         });
     }
+    // Nút xác nhận đặt vé
+    const confirmBtn = this.document.querySelector('.confirm-btn');
+    confirmBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+        if (totalPrice === 0) alert('Vui lòng điền đủ thông tin!');
+        else confirm('Xác nhận đặt vé');
+    });
 });
